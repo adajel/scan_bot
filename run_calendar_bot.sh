fa68c276e344ab804e57397a333122a8cad07666
adajel <ada.joahanne.ellingsrud@gmail.com>
2023-01-31 11:49:06 +0100

    update schedule for calendar bot

--- run_calendar_bot.sh
+++ run_calendar_bot.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
-cd /home/ada/repos/scan_bot/
-python3 calendar_bot.py $1
+
+# run only on odd weeks
+WEEK=`date +"%V"`
+if ! [ $(($WEEK%2)) -eq 0 ];
+then
+    cd /home/ada/repos/scan_bot/
+    python3 calendar_bot.py $1
+fi
