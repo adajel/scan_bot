fefb41dc2aba2201fe09a67952b40ba1b6607438
adajel <ada.joahanne.ellingsrud@gmail.com>
2023-02-27 15:44:47 +0100

    fix bug in bash script for determining if week is even or odd

--- run_calendar_bot.sh
+++ run_calendar_bot.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
 # run only on odd weeks
-#WEEK=`date +"%V"`
-#if ! [ $(($WEEK%2)) -eq 0 ];
-#then
-#    cd /home/ada/repos/scan_bot/
-#    python3 calendar_bot.py $1
-#fi
-cd /home/ada/repos/scan_bot/
-python3 calendar_bot.py $1
-
+WEEK=`date +%V`
+nozero=$(echo $WEEK | sed 's/^0*//')
+if ! [ $(($nozero%2)) -eq 0 ];
+then
+    cd /home/ada/repos/scan_bot/
+    python3 calendar_bot.py $1
+fi
