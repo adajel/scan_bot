ec6c8ed56c5dfcbed4411884a940b22c135a02a7
adajel <ada.joahanne.ellingsrud@gmail.com>
2023-02-27 15:08:25 +0100

    test sheduler calnedar bot

--- run_calendar_bot.sh
+++ run_calendar_bot.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 # run only on odd weeks
-WEEK=`date +"%V"`
-if ! [ $(($WEEK%2)) -eq 0 ];
-then
-    cd /home/ada/repos/scan_bot/
-    python3 calendar_bot.py $1
-fi
+#WEEK=`date +"%V"`
+#if ! [ $(($WEEK%2)) -eq 0 ];
+#then
+#    cd /home/ada/repos/scan_bot/
+#    python3 calendar_bot.py $1
+#fi
+cd /home/ada/repos/scan_bot/
+python3 calendar_bot.py $1
+
