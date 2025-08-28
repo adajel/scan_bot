46254f3fb2b5cb03e4cfc4f996bda9c94297ee38
adajel <97620520+adajel@users.noreply.github.com>
2025-08-28 11:40:00 +0200

    Update calendar_bot.py
    change calendar url

--- calendar_bot.py
+++ calendar_bot.py
@@ -46,7 +46,8 @@ class Message:
             calendar_events
         )
         information = (
-            "y" # "Add new events via the <https://tinyurl.com/5n8zn827|SCAN calendar>."
+            # "Add new events via the <https://tinyurl.com/5n8zn827|SCAN calendar>."
+            "Add new events via the <https://calendar.google.com/calendar/u/0/r?cid=c2ltdWxhLm5vXzlnYTdydHQwMnBqY250bG9rMTY4ODZycGprQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20|SCAN calendar>."
         )
         return self._get_task_block(text, information)
 
